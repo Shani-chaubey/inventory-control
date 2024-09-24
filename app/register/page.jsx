e8d39b1b367ff221485d8b5e0106907ea6bcd8c7
Himanshu Chaturvedi <121288199+Shani-chaubey@users.noreply.github.com>
2024-09-24 17:08:21 +0530

    Update page.jsx

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,192 +1,43 @@
-"use client";
-import { signIn, useSession } from "next-auth/react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
+"use client"
+import RegisterForm from "@/components/Auth/RegisterForm";
+import Loader from "@/components/dashboard/Loader";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
-export default function RegisterForm() {
-  const router = useRouter();
-  const { data: session, status } = useSession()
+
+export default function Register() {
+
+  const { status } = useSession()
+  if(status==="loading"){
+    return <Loader />
+  }
   if(status==="authenticated"){
-    router.push('/inventory/home/overview')
+    return redirect('/inventory/home/overview')
   }
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
-  const [loading, setLoading] = useState(false);
-  const [emailErr, setEmailErr] = useState("");
-  async function onSubmit(data) {
-    try {
-      const baseUrl =process.env.NEXT_PUBLIC_BASE_URL
-      setLoading(true);
-      const response = await fetch(`${baseUrl}/api/user`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      const responseData = await response.json();
-
-      if (response.ok) {
-        setLoading(false);
-        toast.success("User Created Successfully");
-        reset();
-        router.push("/login");
-      } else {
-        setLoading(false);
-        if (response.status === 409) {
-          setEmailErr("User with this Email already exists");
-          toast.error("User with this Email already exists");
-        } else {
-          // Handle other errors
-          toast.error("Oops Something Went wrong");
-        }
-      }
-    } catch (error) {
-      setLoading(false);
-      toast.error("Its seems something is wrong with your Network");
-    }
-  }
-
-  const handleSignIn = (provider) => {
-    signIn(provider, {
-      callbackUrl: '/inventory/home/overview' 
-    });
-  };
 
   return (
-    <> 
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="space-y-4 md:space-y-6"
-      action="#"
-    >
-      <div>
-        <label
-          htmlFor="name"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          Your name
-        </label>
-        <input
-          {...register("name", { required: true })}
-          type="text"
-          name="name"
-          id="name"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="John Doe"
-          required=""
-        />
-        {errors.name && (
-          <small className="text-sm text-red-600 ">
-            This field is required
-          </small>
-        )}
+          <img
+            className="w-8 h-8 mr-2"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            alt="logo"
+          />
+          Inventory Management
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Create a new account
+            </h1>
+            <RegisterForm />
+          </div>
+        </div>
       </div>
-      <div>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your email
-        </label>
-        <input
-          {...register("email", { required: true })}
-          type="email"
-          name="email"
-          id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@company.com"
-          required=""
-        />
-        {errors.email && (
-          <small className="text-sm text-red-600 ">
-            This field is required
-          </small>
-        )}
-        <small className="text-sm text-red-600 ">{emailErr}</small>
-      </div>
-      <div>
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Password
-        </label>
-        <input
-          {...register("password", { required: true })}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="••••••••"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required=""
-        />
-        {errors.password && (
-          <small className="text-sm text-red-600 ">
-            This field is required
-          </small>
-        )}
-      </div>
-      {loading ? (
-        <button
-          disabled
-          type="button"
-          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
-        >
-          <svg
-            aria-hidden="true"
-            role="status"
-            className="inline w-4 h-4 mr-3 text-white animate-spin"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="#E5E7EB"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentColor"
-            />
-          </svg>
-          Creating please wait...
-        </button>
-      ) : (
-        <button
-          type="submit"
-          className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Sign Up
-        </button>
-      )}
-
-    </form>
-    <button onClick={()=>handleSignIn('google')} className="w-full text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300
-         font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-            Continue with Google
-      </button>
-      <button onClick={()=>handleSignIn('github')} className="w-full text-white bg-gradient-to-r from-blue-500 via-cyan-500 to-indigo-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300
-         font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-          Continue with Github
-      </button>
-      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-        Already have an account?{" "}
-        <Link
-          href="/login"
-          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        >
-          Login
-        </Link>
-      </p>
-    </>
+    </section>
   );
 }
